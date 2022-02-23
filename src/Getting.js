00123a67a6146535b9683a9d65c7cceaba281f17
unknown <skamisti@gmail.com>
2022-02-23 21:22:05 +0400

    first

---
+++ src/Getting.js
@@ -0,0 +1,10 @@
+const Getting = (props) => {
+    return (
+      <header>
+        <h1>{props.title}</h1>
+        <h5>{props.subtitle}</h5>
+      </header>
+    )
+  };
+
+  export default Getting;
---
+++ src/Welcome.js
@@ -0,0 +1,10 @@
+const Welcome = ({user, age}) => {
+    return (
+        <header>
+            <h1>{user}</h1>
+            <h2>{age}</h2>
+        </header>
+    )
+}
+
+export default Welcome;
